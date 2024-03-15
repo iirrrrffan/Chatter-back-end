b23ae64d1fb5a8afec4bcb378f26a9cf7ee5f69d
iirrrrffan <irfanali376246@gmail.com>
2024-03-15 20:12:46 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+const PORT = 3006
+const cors =require("cors")
+const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users");
+const bodyParser = require("body-parser");
+
+mongoose.connect("mongodb://127.0.0.1:27017/Chatter",{
+useNewUrlParser: true,
+useUnifiedTopology:true
+    
+}).then(()=>{
+    console.log('Connected to MongoDb');
+}).catch((err)=>{
+    console.log('Error connecteing to MongoDb',err);
+})
+
+
+// middleware
+app.use(express.json()); 
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+app.use(cors())
+app.use("/api/auth", authRoute);
+app.use("/api/users",userRoute);
+
+
+
+app.listen(PORT,()=>{
+   console.log('server is runing on port 3000');
+})  
